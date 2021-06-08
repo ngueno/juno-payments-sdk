207341f62b05e4154a4dd7aa7ca6cd3369fd0b8c
Norton Tanner Gueno <nortontgueno@hotmail.com>
2021-06-08 00:48:01 -0300

    Improving client project

--- juno-sdk-autoconfiguration/juno-sdk-client/src/main/java/com/ngueno/juno/sdk/client/JunoSdkClientApplication.java
+++ juno-sdk-autoconfiguration/juno-sdk-client/src/main/java/com/ngueno/juno/sdk/client/JunoSdkClientApplication.java
@@ -1,14 +1,17 @@
 package com.ngueno.juno.sdk.client;
 
+import com.ngueno.juno.sdk.resources.balance.JunoBalanceService;
+import com.ngueno.juno.sdk.resources.charges.JunoChargeService;
+import com.ngueno.juno.sdk.resources.charges.model.JunoChargeListRequest;
+
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
-import com.ngueno.juno.sdk.resources.balance.JunoBalanceService;
-import com.ngueno.juno.sdk.resources.charges.JunoChargeService;
-import com.ngueno.juno.sdk.resources.charges.model.JunoChargeListRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @SpringBootApplication
 public class JunoSdkClientApplication {
 
@@ -18,19 +21,12 @@ public class JunoSdkClientApplication {
                 .web(WebApplicationType.NONE) //
                 .run(args);
 
-        System.out.println();
-        System.out.println();
         JunoBalanceService balanceService = ctxt.getBean(JunoBalanceService.class);
-        System.out.println(balanceService.getBalance());
-        System.out.println();
+        log.info("-- getBalance: {}", balanceService.getBalance());
+
         JunoChargeService chargeService = ctxt.getBean(JunoChargeService.class);
-        System.out.println(chargeService.listCharges(new JunoChargeListRequest()));
-        System.out.println();
-        System.out.println();
-        System.out.println(chargeService.findCharge("chr_6CEE68E7CB97C3B35D2CB439B625D7DC"));
-        System.out.println();
-        System.out.println();
-        chargeService.cancelCharge("chr_6CEE68E7CB97C3B35D2CB439B625D7DC");
+        log.info("-- listCharges: {}", chargeService.listCharges(new JunoChargeListRequest()));
+        log.info("-- findCharge: {}", chargeService.findCharge("chr_6CEE68E7CB97C3B35D2CB439B625D7DC"));
     }
 
 }
